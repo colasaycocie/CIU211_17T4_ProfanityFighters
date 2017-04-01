6dc76361155c86b450e568f200965d6d0656fb13
TristanIbanezFiveSides <1005132@student.sae.edu.au>
2017-04-01 23:22:50 +1100

    Added WIP scritps for Enemy Health and Player attack

---
+++ Assets/My Scripts/EnemyHealth.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public int startingHealth = 100;            // The amount of health the enemy starts the game with.
+    public int currentHealth;                   // The current health the enemy has.
+
+    Animator anim;                              // Reference to the animator.
+    AudioSource enemyAudio;                     // Reference to the audio source.
+
+    bool isDead;                                // Whether the enemy is dead.
+
+    public GameObject[] pickups;
+
+    void Awake()
+    {
+        // Setting up the references.
+        anim = GetComponent<Animator>();
+        enemyAudio = GetComponent<AudioSource>();
+        GameObject.Find("BloodParticle");
+
+        // Setting the current health when the enemy first spawns.
+        currentHealth = startingHealth;
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    public void TakeDamage(int amount)
+    {
+        // If the enemy is dead...
+        if (isDead)
+            // ... no need to take damage so exit the function.
+            return;
+
+        // Play the hurt sound effect.
+        enemyAudio.Play();
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= amount;
+
+        // If the current health is less than or equal to zero...
+        if (currentHealth <= 0)
+        {
+            // ... the enemy is dead.
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        // The enemy is dead.
+        isDead = true;
+
+        // Tell the animator that the enemy is dead.
+        anim.SetTrigger("Dead");
+
+        // Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
+        //enemyAudio.clip = deathClip;
+        enemyAudio.Play();
+        Debug.Log("Dead");
+
+        // disables the objects nav mesh agent
+        GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+
+        // add score to score manager
+        //ScoreManager.score += scoreValue;
+        //ScoreManagerUnlimited.score += scoreValue;
+
+        Destroy(gameObject, 1.5f);
+    }
+}
---
+++ Assets/My Scripts/playerAttack.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAttack : MonoBehaviour {
+
+    public int attackDamage = 15;               // The amount of health taken away per attack.
+
+    Animator anim;                              // Reference to the animator component.
+    GameObject Bullet;                          // Reference to the player GameObject.
+    GameObject enemy;
+    //PlayerHealth playerHealth;                  // Reference to the player's health.
+    EnemyHealth enemyHealth;                    // Reference to this enemy's health.
+    bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
+    float timer;                                // Timer for counting up to the next attack.
+
+    void Awake()
+    {
+        // Setting up the references.
+        Bullet = GameObject.FindGameObjectWithTag("Bullet");
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemyHealth = enemy.GetComponent<EnemyHealth>();
+        anim = enemy.GetComponent<Animator>();
+
+        //anim = GetComponent<Animator>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        // ... the player is in range.
+        Attack();
+
+    }
+
+
+    void Update()
+    {
+
+        // If the player has zero or less health...
+        if (enemyHealth.currentHealth <= 0)
+        {
+            // ... tell the animator the player is dead.
+            anim.SetTrigger("Dead");
+        }
+        anim.SetTrigger("PlayerOutOfRange");
+    }
+
+
+    void Attack()
+    {
+        // If the player has health to lose...
+        if (enemyHealth.currentHealth > 0)
+        {
+            // ... damage the player.
+            enemyHealth.TakeDamage(attackDamage);
+        }
+    }
+}

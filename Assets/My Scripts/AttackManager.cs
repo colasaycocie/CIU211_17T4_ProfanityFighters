b2b470052a5a757f1e893d0f39ecf995f5050048
TristanIbanezFiveSides <1005132@student.sae.edu.au>
2017-04-17 18:04:40 +1000

    Added cooldowns

--- Assets/My Scripts/AttackManager.cs
+++ Assets/My Scripts/AttackManager.cs
@@ -8,10 +8,14 @@ public class AttackManager : MonoBehaviour {
     private int attackDamage;               // The amount of health taken away per attack.
 
     [Header("Cooldowns")]
-    public static float mediumAttack1Cooldown = 10f;
-    public float mediumAttack2Cooldown = 4f;
-    public float mediumAttack3Cooldown = 5f;
-    private bool MA1Cooldown;
+    public static int mediumAttack1Cooldown = 2;
+    public static int mediumAttack2Cooldown = 2;
+    public static int mediumAttack3Cooldown = 2;
+    public static int heavyAttackCooldown = 5;
+    private static bool MA1Cooldown;
+    private static bool MA2Cooldown;
+    private static bool MA3Cooldown;
+    private static bool HACooldown;
 
     Animator anim;                              // Reference to the animator component.
     GameObject enemy;
@@ -89,82 +93,87 @@ public class AttackManager : MonoBehaviour {
             if (EventSystem.current.currentSelectedGameObject.name == "LightAttackButton3")
             {
 
-               StartCoroutine("Attack");
+                StartCoroutine("Attack");
 
-               //activateAttack = false;
+                //activateAttack = false;
 
-               attackDamage = 15;
+                attackDamage = 15;
 
-               instantiatedlightAttack3 = (GameObject)Instantiate(lightAttack3, transform.position, transform.rotation);
+                instantiatedlightAttack3 = (GameObject)Instantiate(lightAttack3, transform.position, transform.rotation);
 
-               // After 2 seconds the text is destroyed.
-               Destroy(instantiatedlightAttack3.gameObject, 2f);
+                // After 2 seconds the text is destroyed.
+                Destroy(instantiatedlightAttack3.gameObject, 2f);
 
             }
 
             if (EventSystem.current.currentSelectedGameObject.name == "MediumAttackButton1" && MA1Cooldown == false)
             {
 
-               StartCoroutine("Attack");
-
-               //activateAttack = false;
-
-               attackDamage = 15;
-
-               instantiatedmediumAttack1 = (GameObject)Instantiate(mediumAttack1, transform.position, transform.rotation);
-
                 MA1Cooldown = true;
+
+                StartCoroutine("Attack");
+
+                //activateAttack = false;
+
+                attackDamage = 15;
+
+                instantiatedmediumAttack1 = (GameObject)Instantiate(mediumAttack1, transform.position, transform.rotation);
+
+
 
                // After 2 seconds the text is destroyed.
                Destroy(instantiatedmediumAttack1.gameObject, 2f);
 
             }
 
-            if (EventSystem.current.currentSelectedGameObject.name == "MediumAttackButton2")
+            if (EventSystem.current.currentSelectedGameObject.name == "MediumAttackButton2" && MA2Cooldown == false)
             {
-    
-               StartCoroutine("Attack");
+                MA2Cooldown = true;
 
-               //activateAttack = false;
+                StartCoroutine("Attack");
 
-               attackDamage = 15;
+                //activateAttack = false;
 
-               instantiatedmediumAttack2 = (GameObject)Instantiate(mediumAttack2, transform.position, transform.rotation);
+                attackDamage = 15;
 
-               // After 2 seconds the text is destroyed.
-               Destroy(instantiatedmediumAttack2.gameObject, 2f);
+                instantiatedmediumAttack2 = (GameObject)Instantiate(mediumAttack2, transform.position, transform.rotation);
+
+                // After 2 seconds the text is destroyed.
+                Destroy(instantiatedmediumAttack2.gameObject, 2f);
 
             }
 
-            if (EventSystem.current.currentSelectedGameObject.name == "MediumAttackButton3")
+            if (EventSystem.current.currentSelectedGameObject.name == "MediumAttackButton3" && MA3Cooldown == false)
             {
+                MA3Cooldown = true;
 
-               StartCoroutine("Attack");
+                StartCoroutine("Attack");
 
-               //activateAttack = false;
+                //activateAttack = false;
 
-               attackDamage = 15;
+                attackDamage = 15;
 
-               instantiatedmediumAttack3 = (GameObject)Instantiate(mediumAttack3, transform.position, transform.rotation);
+                instantiatedmediumAttack3 = (GameObject)Instantiate(mediumAttack3, transform.position, transform.rotation);
 
-               // After 2 seconds the text is destroyed.
-               Destroy(instantiatedmediumAttack3.gameObject, 2f);
+                // After 2 seconds the text is destroyed.
+                Destroy(instantiatedmediumAttack3.gameObject, 2f);
                 
             }
 
-            if (EventSystem.current.currentSelectedGameObject.name == "HeavyAttackButton")
+            if (EventSystem.current.currentSelectedGameObject.name == "HeavyAttackButton" && HACooldown == false)
             {
+                HACooldown = true;
 
-               StartCoroutine("Attack");
+                StartCoroutine("Attack");
 
-               //activateAttack = false;
+                //activateAttack = false;
 
-               attackDamage = 15;
+                attackDamage = 30;
 
-               instantiatedheavyAttack = (GameObject)Instantiate(heavyAttack, transform.position, transform.rotation);
+                instantiatedheavyAttack = (GameObject)Instantiate(heavyAttack, transform.position, transform.rotation);
 
-               // After 2 seconds the text is destroyed.
-               Destroy(instantiatedheavyAttack.gameObject, 2f);
+                // After 2 seconds the text is destroyed.
+                Destroy(instantiatedheavyAttack.gameObject, 2f);
                 
             }
         }
@@ -172,26 +181,64 @@ public class AttackManager : MonoBehaviour {
 
     void Update()
     {
-        Debug.Log(mediumAttack1Cooldown);
+        //Debug.Log(mediumAttack1Cooldown);
+        //Debug.Log("" + activateAttack);
+    }
 
-        if(MA1Cooldown == true)
+    void Cooldowns()
+    {
+        if (MA1Cooldown == true)
         {
-            mediumAttack1Cooldown -= Time.deltaTime;
+            mediumAttack1Cooldown -= 1;
 
-            if(mediumAttack1Cooldown <= 0)
+            if (mediumAttack1Cooldown <= 0)
             {
-                MA1Cooldown = false;
-                mediumAttack1Cooldown = 10f;
-            }
 
-            //if()
+                MA1Cooldown = false;
+                mediumAttack1Cooldown = 2;
+            }
         }
 
-        //Debug.Log("" + activateAttack);
+        if (MA2Cooldown == true)
+        {
+            mediumAttack2Cooldown -= 1;
+
+            if (mediumAttack2Cooldown <= 0)
+            {
+
+                MA2Cooldown = false;
+                mediumAttack2Cooldown = 2;
+            }
+        }
+
+        if (MA3Cooldown == true)
+        {
+            mediumAttack3Cooldown -= 1;
+
+            if (mediumAttack3Cooldown <= 0)
+            {
+
+                MA3Cooldown = false;
+                mediumAttack3Cooldown = 2;
+            }
+        }
+
+        if (HACooldown == true)
+        {
+            heavyAttackCooldown -= 1;
+
+            if (heavyAttackCooldown <= 0)
+            {
+
+                HACooldown = false;
+                heavyAttackCooldown = 5;
+            }
+        }
     }
 
     public IEnumerator Attack()
     {
+        Cooldowns();
         activateAttack = false;
         yield return new WaitForSeconds(2f);
 
